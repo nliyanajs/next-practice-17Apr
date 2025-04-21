aee106aef16c0f97f8126754598d231a16405df3
Nur Liyana JS <nurliyanajs@LOL.local>
2025-04-21 17:06:06 +0800

    21-apr finished streaming and applied implement URL search params

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -3,11 +3,12 @@ import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import { LatestInvoice } from '@/app/lib/definitions';
-export default async function LatestInvoices({
-  latestInvoices,
-}: {
-  latestInvoices: LatestInvoice[];
-}) {
+import { fetchLatestInvoices } from '@/app/lib/data';
+
+
+export default async function LatestInvoices() {
+    const latestInvoices = await fetchLatestInvoices();
+
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -18,6 +19,7 @@ export default async function LatestInvoices({
 
         <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
+            console.log('Mapping Invoice:', invoice); // Insert here
             return (
               <div
                 key={invoice.id}
